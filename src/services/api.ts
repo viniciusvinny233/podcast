d81555f13d1ca813f729f663c2a7754acc948cf1
Marcos Vinicius Pinheiro Rodrigues <81495249+viniciusvinny233@users.noreply.github.com>
2021-05-03 17:24:51 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: 'http://my-json-server.typicode.com/viniciusvinny233/podcast'
+    baseURL: 'http://my-json-server.typicode.com/viniciusvinny233/podcast/'
 })
